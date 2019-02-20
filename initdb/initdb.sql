9cbd1d3dc10c32912116431072e0de34d2459741
Ken Chiang <ken.chiang@grindr.com>
2019-02-20 14:36:58 +0800

    modify sql

--- initdb/initdb.sql
+++ initdb/initdb.sql
@@ -1,13 +1,13 @@
 # ************************************************************
 # Sequel Pro SQL dump
-# Version 5425
+# Version 5427
 #
 # https://www.sequelpro.com/
 # https://github.com/sequelpro/sequelpro
 #
 # Host: 127.0.0.1 (MySQL 8.0.13)
 # Database: bank
-# Generation Time: 2018-12-24 09:31:29 +0000
+# Generation Time: 2019-02-20 06:36:48 +0000
 # ************************************************************
 
 
@@ -24,115 +24,55 @@ SET NAMES utf8mb4;
 # Dump of table Admin
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Admin`;
-
 CREATE TABLE `Admin` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `account` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   `salt` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES `Admin` WRITE;
 /*!40000 ALTER TABLE `Admin` DISABLE KEYS */;
 
 INSERT INTO `Admin` (`id`, `account`, `password`, `salt`)
 VALUES
-	(1,'root','fDhLgvlRr6PYlff2kecwMC2ADdRLlwvds8horN5EYAg=','P2EKEKgfKFr3rPEfEE3E'),
-	(2,'operator','10yNLzv0AD5eLUIIhRuVQpKZvDdk6O/7uxY+Z8gTDK4=','ggWO2EF4EOr3WKPUrKgO'),
-	(3,'manager','xrLrZRKyJaCJK9NRgj4PQxgr2DElhujseaR8oQK8XAw=','OK4KKFPFFFFEFEPFFFPr');
+	(1,'root','fDhLgvlRr6PYlff2kecwMC2ADdRLlwvds8horN5EYAg=','P2EKEKgfKFr3rPEfEE3E');
 
 /*!40000 ALTER TABLE `Admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table AdminRole
+# Dump of table Token
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `AdminRole`;
+CREATE TABLE `Token` (
+  `AdminId` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `Token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`AdminId`),
+  KEY `Token` (`Token`),
+  CONSTRAINT `Token_ibfk_1` FOREIGN KEY (`AdminId`) REFERENCES `Admin` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `AdminRole` (
-  `roleId` int(11) unsigned NOT NULL,
-  `adminId` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`roleId`,`adminId`),
-  KEY `adminId` (`adminId`),
-  CONSTRAINT `AdminRole_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `Admin` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+LOCK TABLES `Token` WRITE;
+/*!40000 ALTER TABLE `Token` DISABLE KEYS */;
 
-LOCK TABLES `AdminRole` WRITE;
-/*!40000 ALTER TABLE `AdminRole` DISABLE KEYS */;
-
-INSERT INTO `AdminRole` (`roleId`, `adminId`)
+INSERT INTO `Token` (`AdminId`, `Token`)
 VALUES
-	(4,1),
-	(2,2),
-	(1,3),
-	(2,3),
-	(3,3);
+	(1,'m0M5Wc1J64SR2HaynDEhDYT7CjE');
 
-/*!40000 ALTER TABLE `AdminRole` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Token` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-# Dump of table Role
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Role`;
-
-CREATE TABLE `Role` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-LOCK TABLES `Role` WRITE;
-/*!40000 ALTER TABLE `Role` DISABLE KEYS */;
-
-INSERT INTO `Role` (`id`, `description`)
-VALUES
-	(1,'transfer user money'),
-	(2,'view transactionLog'),
-	(3,'credit or debit user money'),
-	(4,'super admin');
-
-/*!40000 ALTER TABLE `Role` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table Transaction
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Transaction`;
-
-CREATE TABLE `Transaction` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `fromUserId` int(11) unsigned NOT NULL,
-  `ToUserId` int(11) unsigned DEFAULT NULL,
-  `action` tinyint(2) NOT NULL,
-  `adminId` int(11) unsigned DEFAULT NULL,
-  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `adminId` (`adminId`),
-  KEY `fromUserId` (`fromUserId`),
-  KEY `ToUserId` (`ToUserId`),
-  CONSTRAINT `Transaction_ibfk_1` FOREIGN KEY (`adminId`) REFERENCES `Admin` (`id`),
-  CONSTRAINT `Transaction_ibfk_2` FOREIGN KEY (`fromUserId`) REFERENCES `User` (`id`),
-  CONSTRAINT `Transaction_ibfk_3` FOREIGN KEY (`ToUserId`) REFERENCES `User` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 
 # Dump of table User
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `User`;
-
 CREATE TABLE `User` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `wallet` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
